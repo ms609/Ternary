341480bc28a13263f7b8af61acf9df4cb1d0fae5
Martin R. Smith <martins@gmail.com>
2017-11-23 15:32:29 +0000

    Add colourblind-friendly palettes

--- NEWS.md
+++ NEWS.md
@@ -1,2 +1,5 @@
+# Ternary v0.1.1
+ - Added colour-blind compatible colour palettes `cbPalette8` and `cbPalette15`.
+
 # Ternary v0.1.0
  - Generates ternary plots with vertical left axis.
---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Eight-colour colourblind palette
+#' 
+#' An eight-colour palette compatible with colourblindness.
+#' 
+#' @source \cite{Wong B. 2011. Color blindness. Nat. Methods. 8:441. <doi:10.1038/nmeth.1618>}
+#' @seealso cbPalette15
+#' @keywords datasets
+"cbPalette8"
+
+#' Fifteen-colour colourblind palette
+#' 
+#' A fifteen-colour [Brewer palette](http://mkweb.bcgsc.ca/brewer/) compatible with colourblindness.
+#' 
+#' Note that colour 4 is difficult to distinguish from colour 13 in individuals with tritanopia.
+#' Likewise, colour 7 is difficult to distinguish from colour 3.
+#' 
+#' @source http://mkweb.bcgsc.ca/biovis2012/color-blindness-palette.png
+#' @keywords datasets
+"cbPalette15"
+
