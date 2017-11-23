36e4109515b81ec79e6812b6995cee4a7858c795
Martin R. Smith <martins@gmail.com>
2017-11-23 14:05:16 +0000

    Update tests & cran details

--- cran-comments.md
+++ cran-comments.md
@@ -3,7 +3,15 @@
 * ubuntu 12.04 (on travis-ci), R 3.2.0 and devel
 
 ## R CMD check results
-There were no ERRORs, WARNINGs or NOTEs.
+There were no ERRORs or WARNINGs.
+
+There was one NOTE:
+
+Maintainer: 'Martin R. Smith <martins@gmail.com>'
+
+New submission
+
+
 
 Because the package implements a widely-used graphical technique, there is 
 no obvious reference that needs to be included in the DESCRIPTION file.
--- tests/testthat/test-ternary.R
+++ tests/testthat/test-ternary.R
@@ -20,8 +20,14 @@ test_that("TernaryCoords gives correct coordinates", {
     c(sin(pi/3)/2, 0)
   )
   expect_equal(output, lapply(input, TernaryCoords))
+  expect_error(TernaryCoords(rep(1, 5), 1, 1))
+  expect_equal(c(0, 0.5), TernaryCoords(1, 0, 0))
+})
+
+test_that("Ternary plotting does not fail", {
   expect_null(TernaryPlot('A', 'B', 'C'))
   expect_null(HorizontalGrid())
+  expect_error(TernaryPoints(rep(1, 5)))
   expect_null(TernaryPoints(c(1,1,1)))
   expect_null(TernaryText(c(1,1,1), 'A'))
   expect_null(TernaryLines(list(c(1,1,1), c(0, 1, 2)), lwd=2))
