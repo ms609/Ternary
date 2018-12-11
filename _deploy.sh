287efefa66ca1b69bf37bc9744282b00dd113e49
Martin Smith <martins@gmail.com>
2018-12-11 08:50:47 +0000

    ${GITHUB_PAT} is necessary
    Thank goodness...

--- _deploy.sh
+++ _deploy.sh
@@ -6,10 +6,10 @@ set -e
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 git config --global user.email "martins@gmail.com"
 git config --global user.name "Martin Smith"
-git clone -b gh-pages https://github.com/${TRAVIS_REPO_SLUG}.git render-output
-Rscript -e "devtools::build_manual('.', './render-output')"
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git render-output
 cd render-output
+Rscript -e "devtools::build_manual('.', '.')"
 # cp -r ../doc/* ./
 git add --all *
-git commit -m "Render documentation" || true
+git commit -m"Render documentation" || true
 git push -q origin gh-pages
