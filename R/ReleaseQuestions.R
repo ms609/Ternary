10ec3ac3b9905bf91c7d0b65a1471d7ad8d56438
Martin Smith <martins@gmail.com>
2019-09-12 11:45:05 +0100

    Clear branch

--- R/ReleaseQuestions.R
+++
@@ -1,6 +0,0 @@
-release_questions <- function() {
-  c(
-    "Are vignettes up to date?",
-    "Have you updated .zenodo.json?"
-  )
-}
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(Ternary)
-
-test_check("Ternary")
