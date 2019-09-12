79660892cddac93f381ccd1a6151bec849459a93
Martin Smith <martins@gmail.com>
2019-09-12 15:11:46 +0100

    Extra tests

--- R/ReleaseQuestions.R
+++ R/ReleaseQuestions.R
@@ -4,3 +4,12 @@ release_questions <- function() {
     "Have you updated .zenodo.json?"
   )
 }
+
+
+# Additional tests:
+# 
+# check_win_devel(); check_rhub()
+# revdepcheck::revdep_check()
+# build_vignettes()
+# tools::resaveRdaFiles('data', compress='auto') - is default bzip2 the optimal?
+# tools::checkRdaFiles('data') - set optimal compression in `data-raw`
