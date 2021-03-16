c6fa2686f997efe09cc3ac1b9074fea45e95e8d6
Martin Smith <martins@gmail.com>
2021-03-16 16:02:05 +0000

    Test plotting

--- tests/testthat/test-Holdridge.R
+++ tests/testthat/test-Holdridge.R
@@ -4,3 +4,16 @@ test_that("Holdridge coordinate transformation", {
   tern <- cbind(c(1, 0, 0), c(0, 0, 1), c(0, 1, 1), c(0, 1, 0))
   expect_equal(TernaryToXY(tern), HoldridgeToXY(pet, prec))
 })
+
+test_that("Holdridge plotting", {
+  HoldridgeBasic <- function () {
+    HoldridgePlot()
+    pet <- holdridge[, 2]
+    prec <- holdridge$Precipition
+    HoldridgePoints(pet, prec, cex = 2)
+    HoldridgeText(pet, prec, seq_along(pet[-12]), cex = 0.6)
+  }
+  
+  skip_if_not_installed('vdiffr')
+  vdiffr::expect_doppelganger('holdridge-basic', HoldridgeBasic)
+})
