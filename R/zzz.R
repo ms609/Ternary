c80d21e19fc9e9cd5f0e242ace2748bc0bac309f
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-01-04 20:42:16 +0000

    rm redundant checks

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,15 @@
 release_questions <- function() {
   c(
-    "Are any new features described in the vignette?"
+    "Are any new features described in the vignettes?"
   )
 }
 
 
 # Additional tests:
 #
-# spell_check()
 # codemeta::write_codemeta()
-# pkgdown::build_reference_index()
 # check_win_devel(); rhub::check_for_cran()
-# revdepcheck::revdep_check()
 # build_vignettes()
+# 
 # tools::resaveRdaFiles('data', compress = 'auto') - is default of bzip2 the optimal?
 # tools::checkRdaFiles('data') - set optimal compression in `data-raw`
