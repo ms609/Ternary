7e94080d4b2b15094d80943062b44728db90f88f
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2024-01-03 09:04:55 +0000

    wrap

--- man-roxygen/directionParam.R
+++ man-roxygen/directionParam.R
@@ -1,2 +1,2 @@
-#' @param direction (optional) Integer specifying the direction that the current ternary plot 
-#'                  should point: 1, up; 2, right; 3, down; 4, left.
+#' @param direction (optional) Integer specifying the direction that the
+#'  current ternary plot should point: 1, up; 2, right; 3, down; 4, left.
